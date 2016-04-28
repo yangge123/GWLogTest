92e7a6319e9e534f1ec7e3b015e0dbc03c392c4d
“yangye” <yang_ye@gowild.cn>
2016-04-28 20:06:29 +0800

    add

---
+++ CustomAlertView/Demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo1
+//
+//  Created by gowild on 16/4/21.
+//  Copyright © 2016年 gowild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAlertView/Demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo1
+//
+//  Created by gowild on 16/4/21.
+//  Copyright © 2016年 gowild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
