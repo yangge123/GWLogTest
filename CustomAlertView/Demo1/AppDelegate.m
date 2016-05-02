c385f5191539751f3fc1f0daf999c9fcdf1205f7
“yangye” <yang_ye@gowild.cn>
2016-05-02 14:59:55 +0800

    m

--- CustomAlertView/Demo1/AppDelegate.m
+++ CustomAlertView/Demo1/AppDelegate.m
@@ -17,7 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
+    
+    //master gitignore 
     return YES;
 }
 
