060a7412ff18482c23c60d83b2c2ae67fa2a0121
“yangye” <yang_ye@gowild.cn>
2016-05-05 17:43:36 +0800

    add kissXML

---
+++ kissXML/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by gowild on 16/5/5.
+//  Copyright © 2016年 gowild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kissXML/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by gowild on 16/5/5.
+//  Copyright © 2016年 gowild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
